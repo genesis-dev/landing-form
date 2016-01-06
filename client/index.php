de54c606a64ff112a245bfd781bf76c2cb12eb78
glmeist <e@genesis.kz>
2016-01-06 15:22:05 +0600

    Создана клиентская часть

---
+++ client/index.php
@@ -0,0 +1,22 @@
+<?php
+    header('Content-Type: application/javascript');
+    $config = include __DIR__."/../config/local.php";
+    if (isset($config['sites'][$_GET['siteID']])):
+?>
+(function() {
+    var landingForm = {
+        siteID: "<?= $_GET['siteID'] ?>",
+        selector: ".landing-form"
+    };
+
+    window.onsubmit = function (e) {
+        console.log(e);
+    }
+})();
+<?php
+    else:
+?>
+alert("Invalid siteID");
+<?php
+    endif;
+?>
