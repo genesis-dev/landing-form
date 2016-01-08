bd520f4725e83d1b66e44b54cb3b1951117b2f1b
glmeist <e@genesis.kz>
2016-01-08 15:25:39 +0600

    Вывод ошибок валидации в ответе

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ try {
     //$origin = $form->getSiteConfig()['origin'];
     //header("Access-Control-Allow-Origin: http://genesis.kz");
     if ($form->load() && $form->validate()) {
-        $data['valid'] = true;
+
         if($form->send())
             $data['success'] = true;
         $form->save();
     } else {
+        $data['valid'] = false;
         $data->errors = $form->errors;
     }
 } catch (Exception $e) {
