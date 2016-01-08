b8c864629285275b60b7dc5e8867b241b491ad43
glmeist <e@genesis.kz>
2016-01-08 15:01:36 +0600

    В ответ добавлен заголовок Access-Control-Allow-Origin

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 header('Content-Type: application/json');
 require __DIR__."/LandingForm.php";
-$data = array("foo"=>$_SERVER['HTTP_ORIGIN']);
+$data = array();
 try {
-    //$form = new LandingForm();
+    $form = new LandingForm();
     //$origin = $form->getSiteConfig()['origin'];
-    header("Access-Control-Allow-Origin: http://genesis.kz");
+    //header("Access-Control-Allow-Origin: http://genesis.kz");
     $data['success'] = true;
-    /*if ($form->load() && $form->validate()) {
+    if ($form->load() && $form->validate()) {
         $form->send();
         $form->save();
-    }*/
+    }
 } catch (Exception $e) {
     $data['success'] = false;
     $data['errors'] = [$e->getMessage()];
