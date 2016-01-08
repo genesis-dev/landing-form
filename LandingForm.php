91be85cce738dd5fdab824117f8a2ff1c30c2752
glmeist <e@genesis.kz>
2016-01-08 17:30:41 +0600

    Отладка отправки данных в базу данных

--- LandingForm.php
+++ LandingForm.php
@@ -47,7 +47,7 @@ class LandingForm {
         }
         $mail->Subject = $this->siteConfig['mailer']['subject'];
         $mail->Body = "Проверка";
-        ob_start();
+        ob_start(null, 0, PHP_OUTPUT_HANDLER_STDFLAGS ^ PHP_OUTPUT_HANDLER_CLEANABLE);
         include __DIR__."/views/mailBody.php";
         $mail->Body = ob_get_contents();
         $mail->isHTML(true);
