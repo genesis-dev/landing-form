47004ee591338ffae94802ebb44910365744b118
glmeist <e@genesis.kz>
2016-01-08 16:01:51 +0600

    Отладка отправки данных на почтовый ящик

--- LandingForm.php
+++ LandingForm.php
@@ -62,7 +62,7 @@ class LandingForm {
      * @return number
      */
     public function save() {
-        return $this->database->insert($this->config->table, ["data" => $this->fiedls]);
+        return $this->database->insert($this->config["table"], ["data" => $this->fiedls]);
     }
 
     /**
