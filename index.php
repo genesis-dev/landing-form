ffba35bd8d30acbb112085cfc80a4bcfd0ade9c8
glmeist <e@genesis.kz>
2016-01-08 17:56:02 +0600

    Отладка валидации

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ try {
     //$origin = $form->getSiteConfig()['origin'];
     //header("Access-Control-Allow-Origin: http://genesis.kz");
     $form->load();
+    $data['conf'] = $form->getSiteConfig();
     if ($form->validate()) {
         if($form->send())
             $data['success'] = true;
