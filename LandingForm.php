1f89008984821a302b81b889db36ed7a5fc2f5b5
glmeist <e@genesis.kz>
2016-01-08 18:28:55 +0600

    Исправлена ошибка при валидации

--- LandingForm.php
+++ LandingForm.php
@@ -71,7 +71,7 @@ class LandingForm {
     public function validate() {
         $this->errors = [];
         foreach($this->fields as $key=>$field) {
-            if (is_callable($this->siteConfig['validators'][$key]) && !$this->siteConfig['validators'][$key]($field['value'], $this->fields, $this->siteConfig['validators']))
+            if (is_callable($this->siteConfig['validators'][$key]) && !$this->siteConfig['validators'][$key]($field['value'], null, null))
                 $this->errors[] = $key;
         }
         if (!empty($this->errors))
