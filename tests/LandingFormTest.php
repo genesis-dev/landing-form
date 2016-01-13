ebdf1407b9d668dfb5d112e2b5289559deb67d73
glmeist <e@genesis.kz>
2016-01-13 15:30:47 +0600

    Добавлен тест валидации

--- tests/LandingFormTest.php
+++ tests/LandingFormTest.php
@@ -16,13 +16,16 @@ class LandingFormTest extends PHPUnit_Framework_TestCase
         $_GET['landingForm']['email'] = 'glmeist@gmail.com';
         $_GET['landingForm']['phone'] = '+77012240824';
         $valid_form = new LandingForm();
+        $valid_form->load();
         $this->assertTrue($valid_form->validate());
         $_GET['landingForm']['phone'] = '87012240824';
         $valid_form2 = new LandingForm();
+        $valid_form2->load();
         $this->assertTrue($valid_form2->validate());
         $_GET['landingForm']['email'] = 'not_email';
         $_GET['landingForm']['phone'] = '+77012240824321';
         $invalid_form = new LandingForm();
+        $invalid_form->load();
         $this->assertFalse($invalid_form->validate());
     }
 }
