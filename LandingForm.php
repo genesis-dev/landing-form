f1af48125611b31f11e4a38b9592ecdb0e4b9a54
glmeist <e@genesis.kz>
2016-01-13 17:04:13 +0600

    Добавлен тест отправки и сохранения

--- LandingForm.php
+++ LandingForm.php
@@ -60,10 +60,16 @@ class LandingForm {
 
     public function sendTelegram() {
         $api_key = $this->config["telegram"]["api_key"];
-        $url = "https://api.telegram.org/bot$api_key/getUpdates";
+        ob_start();
+        include __DIR__."/views/mailAltBody.php";
+        $text = ob_get_contents();
+        ob_end_clean();
+        $url = "https://api.telegram.org/bot$api_key/sendMessage?text=$text&chat_id=glmeist";
         $handle = curl_init($url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-        return $this->exec_curl_request($handle);
+        curl_setopt ($handle, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($handle);
+        curl_close($handle);
+        return $response;
     }
 
     private function exec_curl_request($handle) {
