1e27be2815ba12ae6740a5cbd5828b4c742ef820
glmeist <e@genesis.kz>
2016-01-13 15:15:17 +0600

    Добавлен тест проверки siteID

---
+++ tests/LandingFormTest.php
@@ -0,0 +1,14 @@
+<?php
+require "../LandingForm.php";
+class LandingFormTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Unknown resource.
+     */
+    public function testExceptionHasRightSiteID()
+    {
+        $_GET['siteID'] = "some other ID";
+        $form = new LandingForm();
+    }
+}
