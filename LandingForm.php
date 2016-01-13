8f1dfde3b7a6aa53b9628e642643359550c91d0b
glmeist <e@genesis.kz>
2016-01-13 17:38:41 +0600

    Добавлена функция для отправки в Телеграм

--- LandingForm.php
+++ LandingForm.php
@@ -62,7 +62,7 @@ class LandingForm {
         $api_key = $this->config["telegram"]["api_key"];
         ob_start();
         include __DIR__."/views/telegram.php";
-        $text = ob_get_contents();
+        $text = urlencode(ob_get_contents());
         ob_end_clean();
         $url = "https://api.telegram.org/bot$api_key/sendMessage?text=$text&chat_id=@genesis_orders";
         $handle = curl_init($url);
