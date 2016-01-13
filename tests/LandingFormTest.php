0f336112303efa3418ca38b7a0602ff6d5263b46
glmeist <e@genesis.kz>
2016-01-13 15:29:15 +0600

    Добавлен тест валидации

--- tests/LandingFormTest.php
+++ tests/LandingFormTest.php
@@ -10,4 +10,19 @@ class LandingFormTest extends PHPUnit_Framework_TestCase
         $_GET['siteID'] = "some other ID";
         $form = new LandingForm();
     }
+
+    public function testValidateEmailAndPhone() {
+        $_GET['siteID'] = "h9apn";
+        $_GET['landingForm']['email'] = 'glmeist@gmail.com';
+        $_GET['landingForm']['phone'] = '+77012240824';
+        $valid_form = new LandingForm();
+        $this->assertTrue($valid_form->validate());
+        $_GET['landingForm']['phone'] = '87012240824';
+        $valid_form2 = new LandingForm();
+        $this->assertTrue($valid_form2->validate());
+        $_GET['landingForm']['email'] = 'not_email';
+        $_GET['landingForm']['phone'] = '+77012240824321';
+        $invalid_form = new LandingForm();
+        $this->assertFalse($invalid_form->validate());
+    }
 }
